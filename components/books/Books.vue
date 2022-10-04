e31b432edeb58542f3fc6a2480e2b1e32b0d48bb
Oliver <47889937+otd1990@users.noreply.github.com>
2022-10-04 12:30:37 +0100

    Add files via upload

---
+++ components/books/Books.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="books">
+    <div v-if="data">
+      <div v-for="book in data" :key="book.id">
+        <img :src="book.image" :alt="book.title" />
+        <div>{{ book.title }}</div>
+        <div>{{ book.desc }}</div>
+      </div>
+    </div>
+    <div v-else>Loading</div>
+  </div>
+</template>
+
+<script setup>
+const supabase = useSupabaseClient();
+let { data } = await supabase.from("books").select(`*`);
+</script>
