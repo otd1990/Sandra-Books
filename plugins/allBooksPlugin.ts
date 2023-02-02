4abae0f4a3ddc1cc6e9dadc13847d032175eef15
Oliver Dyer <Oliver.Dyer@dominos.co.uk>
2023-02-02 16:05:00 +0000

    Removing console log

--- plugins/allBooksPlugin.ts
+++ plugins/allBooksPlugin.ts
@@ -3,7 +3,6 @@ import { useBooksStore } from "@/store/BooksStore";
 
 export default defineNuxtPlugin(async (nuxtApp) => {
   const bookStore = useBooksStore(nuxtApp.$pinia);
-  console.log("all books plugin");
   await bookStore.getBooksFromServ();
   await bookStore.getApprovedReviews();
 });
